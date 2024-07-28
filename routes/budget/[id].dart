89b6bf6c5bf6ba09c9d76b792b7ab2daede0c76e
posthello-code <github@alexjensen.net>
2024-07-28 12:58:54 -0700

    handle allowed operations for dynamic budget route

--- routes/budget/[id].dart
+++ routes/budget/[id].dart
@@ -1,13 +1,30 @@
 import 'dart:convert';
 
 import 'package:dart_frog/dart_frog.dart';
+import 'package:mongo_dart/mongo_dart.dart';
 
-Response onRequest(RequestContext context, String id) {
+Future<Response> onRequest(RequestContext context, String id) async {
+  final db = context.read<Db>();
+
+  Future<Map<String, dynamic>?> getById(String id) {
+    return db
+        .collection('budgets')
+        .findOne({'_id': ObjectId.fromHexString(id)});
+  }
+
   if (context.request.method.toString() == 'HttpMethod.get') {
-    return Response(body: jsonEncode({'data': 'get response'}));
+    final r = await getById(id);
+    return Response(body: jsonEncode(r));
   }
   if (context.request.method.toString() == 'HttpMethod.put') {
-    return Response(body: jsonEncode({'data': 'put response'}));
+    final body = await context.request.body();
+    final json = jsonDecode(body) as Map<String, dynamic>;
+    // ignore: cascade_invocations
+    json.remove('id');
+    await db
+        .collection('budgets')
+        .replaceOne({'_id': ObjectId.fromHexString(id)}, json);
+    return Response(body: jsonEncode(json));
   }
   return Response(statusCode: 405);
 }
