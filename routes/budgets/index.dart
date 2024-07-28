d6b418440b847237ec068f8f7768854f3599bb29
posthello-code <github@alexjensen.net>
2024-07-28 13:02:40 -0700

    handle the budgets route (create only)

---
+++ routes/budgets/index.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+import 'package:dart_frog/dart_frog.dart';
+import 'package:mongo_dart/mongo_dart.dart';
+
+Future<Response> onRequest(RequestContext context) async {
+  final db = context.read<Db>();
+
+  if (context.request.method.toString() == 'HttpMethod.post') {
+    final body = await context.request.body();
+    final json = jsonDecode(body) as Map<String, dynamic>;
+    await db.collection('budgets').insertOne(json);
+    return Response(body: jsonEncode(json));
+  }
+  return Response(statusCode: 405);
+}
