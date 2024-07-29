34533f364cf4be167bf35a7d422f13eef1d2a21d
posthello-code <github@alexjensen.net>
2024-07-28 18:50:05 -0700

    refactor dynamic budgets route

--- routes/budgets/[id].dart
+++ routes/budgets/[id].dart
@@ -5,26 +5,19 @@ import 'package:mongo_dart/mongo_dart.dart';
 
 Future<Response> onRequest(RequestContext context, String id) async {
   final db = context.read<Db>();
-
-  Future<Map<String, dynamic>?> getById(String id) {
-    return db
-        .collection('budgets')
-        .findOne({'_id': ObjectId.fromHexString(id)});
-  }
-
-  if (context.request.method.toString() == 'HttpMethod.get') {
-    final r = await getById(id);
+  final method = context.request.method.toString();
+  final collection = db.collection('budgets');
+  final oid = ObjectId.fromHexString(id);
+  if (method == 'HttpMethod.get') {
+    final r = await collection.findOne({'_id': oid});
     return Response(body: jsonEncode(r));
   }
-  if (context.request.method.toString() == 'HttpMethod.put') {
+  if (method == 'HttpMethod.put') {
     final body = await context.request.body();
     final json = jsonDecode(body) as Map<String, dynamic>;
-    // ignore: cascade_invocations
     json.remove('id');
-    await db
-        .collection('budgets')
-        .replaceOne({'_id': ObjectId.fromHexString(id)}, json);
+    await collection.replaceOne({'_id': oid}, json);
     return Response(body: jsonEncode(json));
   }
-  return Response(statusCode: 405);
+  return Response(statusCode: 500);
 }
