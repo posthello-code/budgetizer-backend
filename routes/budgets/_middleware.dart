d594f71a6521eeddcf2e6f37721eaa1a0d227eb8
posthello-code <github@alexjensen.net>
2024-07-28 23:59:34 -0700

    localhost allowed

--- routes/budgets/_middleware.dart
+++ routes/budgets/_middleware.dart
@@ -6,8 +6,7 @@ Handler middleware(Handler handler) {
         fromShelfMiddleware(
           shelf.corsHeaders(
             headers: {
-              shelf.ACCESS_CONTROL_ALLOW_ORIGIN:
-                  'https://budgetizer.onrender.com',
+              shelf.ACCESS_CONTROL_ALLOW_HEADERS: 'http://localhost*',
             },
           ),
         ),
