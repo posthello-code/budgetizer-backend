52226b4f1e15a7e724747a8899821093544c5821
posthello-code <github@alexjensen.net>
2024-07-29 01:04:59 -0700

    wildcard allow headers

--- routes/budgets/_middleware.dart
+++ routes/budgets/_middleware.dart
@@ -6,7 +6,7 @@ Handler middleware(Handler handler) {
         fromShelfMiddleware(
           shelf.corsHeaders(
             headers: {
-              shelf.ACCESS_CONTROL_ALLOW_HEADERS: 'http://localhost*',
+              shelf.ACCESS_CONTROL_ALLOW_HEADERS: '*',
             },
           ),
         ),
