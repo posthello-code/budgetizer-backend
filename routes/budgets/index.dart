5e97368a3f6a43c69533133d0a945c2c921cd797
posthello-code <github@alexjensen.net>
2024-07-28 19:25:59 -0700

    allow updates via post when ID provided

--- routes/budgets/index.dart
+++ routes/budgets/index.dart
@@ -4,13 +4,24 @@ import 'package:mongo_dart/mongo_dart.dart';
 
 Future<Response> onRequest(RequestContext context) async {
   final db = context.read<Db>();
+  final collection = db.collection('budgets');
+  final body = await context.request.body();
+  final json = jsonDecode(body) as Map<String, dynamic>;
 
-  if (context.request.method.toString() == 'HttpMethod.post') {
-    final body = await context.request.body();
-    final json = jsonDecode(body) as Map<String, dynamic>;
+  // Handle updates
+  if (json.keys.any((key) => key == 'id') &&
+      context.request.method.toString() == 'HttpMethod.post') {
+    final existingOid = ObjectId.fromHexString(json['id'] as String);
     json.remove('id');
-    await db.collection('budgets').insertOne(json);
+    await collection.replaceOne({'_id': existingOid}, json);
     return Response(body: jsonEncode(json));
   }
+
+  // Handle creates
+  if (context.request.method.toString() == 'HttpMethod.post') {
+    await collection.insertOne(json);
+    return Response(body: jsonEncode(json));
+  }
+
   return Response(statusCode: 405);
 }
