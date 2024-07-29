5fc0c043b02123064abf26a81d6014f5f2fe607e
posthello-code <github@alexjensen.net>
2024-07-28 23:30:27 -0700

    middleware for cors

--- routes/budgets/_middleware.dart
+++ routes/budgets/_middleware.dart
@@ -1,10 +1,15 @@
 import 'package:dart_frog/dart_frog.dart';
+import 'package:shelf_cors_headers/shelf_cors_headers.dart' as shelf;
 
 Handler middleware(Handler handler) {
-  return (context) async {
-    // do something before
-    final response = await handler(context);
-    // do something after
-    return response;
-  };
+  return handler.use(requestLogger()).use(
+        fromShelfMiddleware(
+          shelf.corsHeaders(
+            headers: {
+              shelf.ACCESS_CONTROL_ALLOW_ORIGIN:
+                  'https://budgetizer.onrender.com',
+            },
+          ),
+        ),
+      );
 }
